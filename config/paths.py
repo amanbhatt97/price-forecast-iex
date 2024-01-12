829f9ba53627de54ebcf7f8015fab090e89f7861
aman.bhatt <aman.bhatt@climateconnect.digital>
2024-01-12 21:08:29 +0530

    added paths

--- config/paths.py
+++ config/paths.py
@@ -5,7 +5,7 @@ Author: Aman Bhatt
 """
 
 from pathlib import Path
-import sys
+import sys, os
 
 # Parent directory
 script_dir = Path(__file__).resolve().parent
@@ -29,21 +29,22 @@ class ProjectPaths:
         self.src = parent_directory / 'src'  # Source scripts
         self.deploy = parent_directory / 'deploy'  # Forecasting/reports scripts
         self.config = parent_directory / 'config'  # Configuration files
-        self.logs = parent_directory / 'logs'  # Logs
+        self.logs = parent_directory / 'logs'       # Logs
+        self.models = parent_directory / 'models'   # saved models 
         self.forecasts_dam = parent_directory / 'forecasts' / 'day_ahead'  # DAM forecast files
         self.forecasts_dir = parent_directory / 'forecasts' / 'directional'  # Directional forecast files
         self.reports_dam = parent_directory / 'reports' / 'day_ahead'  # DAM reports
         self.reports_dir = parent_directory / 'reports' / 'directional'  # Directional reports
 
         # Create directories if they do not exist
-        self.create_directories()
+        self._create_directories()
 
-    def create_directories(self):
+    def _create_directories(self):
         """
         Create necessary directories if they do not exist.
         """
         directories = [
-            self.data, self.src, self.deploy, self.config, self.logs,
+            self.data, self.src, self.deploy, self.config, self.logs, self.models,
             self.forecasts_dam, self.forecasts_dir, self.reports_dam, self.reports_dir
         ]
 
@@ -56,15 +57,17 @@ class ProjectPaths:
 project_paths = ProjectPaths(project_dir)
 
 # Data path
-data_path = project_paths.data
+raw_data_path = str(project_paths.data / 'raw')
+processed_data_path = str(project_paths.data / 'processed')
+external_data_path = str(project_paths.data / 'external')
 
-# Logs path
-log_path = project_paths.logs
+# # Logs path
+# log_path = project_paths.logs
 
-# Forecast paths
-dam_forecast_path = project_paths.forecasts_dam
-dir_forecast_path = project_paths.forecasts_dir
+# # Forecast paths
+# dam_forecast_path = project_paths.forecasts_dam
+# dir_forecast_path = project_paths.forecasts_dir
 
-# Accuracy reports path
-dam_reports_path = project_paths.reports_dam
-dir_reports_path = project_paths.reports_dir
+# # Accuracy reports path
+# dam_reports_path = project_paths.reports_dam
+# dir_reports_path = project_paths.reports_dir
