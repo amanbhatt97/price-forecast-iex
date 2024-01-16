e527f9c9095571a464009fcffa0854efaca72e80
aman.bhatt <aman.bhatt@climateconnect.digital>
2024-01-17 02:28:19 +0530

    improved script

--- src/feature_engineering/build_features.py
+++ src/feature_engineering/build_features.py
@@ -1,4 +1,3 @@
-# dependencies
 import pandas as pd
 import numpy as np
 import math
@@ -9,24 +8,21 @@ class FeatureEngineering:
     def __init__(self, PROJECT_PATH):
         self.PROJECT_PATH = PROJECT_PATH
 
-
     def shift_date(self, df, n):
         """ This method shifts dataframe df by n days downwards"""
         shifted_df = df.copy()
         try:
-            shifted_df['datetime'] = shifted_df['datetime'] + pd.DateOffset(days=n)
+            shifted_df['datetime'] += pd.DateOffset(days=n)
         except:
-            shifted_df['date'] = shifted_df['date'] + pd.DateOffset(days=n) 
+            shifted_df['date'] += pd.DateOffset(days=n)
 
         return shifted_df
-
 
     def merge_dataframes(self, dfs, on_column='datetime'):
         """This method merges all the dataframes in a list dfs"""
         merged_df = reduce(lambda left, right: pd.merge(left, right, on=on_column, how='left'), dfs)
         merged_df.dropna(inplace=True)
         return merged_df
-    
 
     def _capping(self, data):
         conditions = [
@@ -38,135 +34,97 @@ class FeatureEngineering:
         data['capping'] = np.select(conditions, capping_values)
         return data
 
-
     def _datetime_features(self, df):
-        data = df.copy()
-        # time related features
-        data['date'] = data.datetime.dt.date     
-        data['date'] = pd.to_datetime(data['date'])
-        data['hour'] = data.datetime.dt.hour + 1   
-        data['dom' ] = data.datetime.dt.day        
-        data['month'] = data.datetime.dt.month     
-        data['year'] = data.datetime.dt.year       
-        data['dow' ] = data.datetime.dt.dayofweek  
-        data['doy' ] = data.datetime.dt.dayofyear  
-        data['tb'] = data.datetime.apply(lambda x : ((x.hour*60 + x.minute)//15+1)) 
-        data['next_day_sunday'] = np.where(data['dow']==5, 1,0) 
-        data['hour_dow'] = data['hour'] * data['dow']
+        df['date'] = pd.to_datetime(df['datetime'].dt.date)
+        df['hour'] = df['datetime'].dt.hour + 1
+        df['dom'] = df['datetime'].dt.day
+        df['month'] = df['datetime'].dt.month
+        df['year'] = df['datetime'].dt.year
+        df['dow'] = df['datetime'].dt.dayofweek
+        df['doy'] = df['datetime'].dt.dayofyear
+        df['tb'] = ((df['datetime'].dt.hour * 60 + df['datetime'].dt.minute) // 15 + 1)
+        df['next_day_sunday'] = np.where(df['dow'] == 5, 1, 0)
+        df['hour_dow'] = df['hour'] * df['dow']
 
-        # time of day features
-        data['isMorning'] = ((data['datetime'].dt.hour>=6) &  (data['datetime'].dt.hour<=9) ).astype(int)
-        data['isDay'] = ((data['datetime'].dt.hour>=10) &  (data['datetime'].dt.hour<=16) ).astype(int)
-        data['isEvening'] = ((data['datetime'].dt.hour>=17) &  (data['datetime'].dt.hour<=23) ).astype(int)
-        data['isNight'] = ((data['datetime'].dt.hour==24) | ((data['datetime'].dt.hour>=1) & (data['datetime'].dt.hour<=5)) ).astype(int)
+        df['isMorning'] = ((df['datetime'].dt.hour >= 6) & (df['datetime'].dt.hour <= 9)).astype(int)
+        df['isDay'] = ((df['datetime'].dt.hour >= 10) & (df['datetime'].dt.hour <= 16)).astype(int)
+        df['isEvening'] = ((df['datetime'].dt.hour >= 17) & (df['datetime'].dt.hour <= 23)).astype(int)
+        df['isNight'] = ((df['datetime'].dt.hour == 24) | ((df['datetime'].dt.hour >= 1) & (df['datetime'].dt.hour <= 5))).astype(int)
 
-        # season features
-        data['winter'] = ((data['month'] == 12) | (data['month'] == 1) | (data['month'] == 2)).astype(int)
-        data['summer'] = ((data['month'] >= 3) & (data['month'] <= 6)).astype(int)
-        data['monsoon'] = ((data['month'] >= 7) & (data['month'] <= 8)).astype(int)
-        data['autumn'] = ((data['month'] >= 9) & (data['month'] == 11)).astype(int)
+        df['winter'] = ((df['month'] == 12) | (df['month'] == 1) | (df['month'] == 2)).astype(int)
+        df['summer'] = ((df['month'] >= 3) & (df['month'] <= 6)).astype(int)
+        df['monsoon'] = ((df['month'] >= 7) & (df['month'] <= 8)).astype(int)
+        df['autumn'] = ((df['month'] >= 9) & (df['month'] == 11)).astype(int)
 
-        return data
-
+        return df
 
     def _target(self, data, market_type):
-        if market_type == 'dam':
-            data['target'] = data[f'mcp_{market_type}'].shift(-96)
-        elif market_type == 'rtm':
-            data['target'] = data[f'mcp_{market_type}'].shift(-96*2)
-        else:
-            print('Chose dam or rtm.')
+        data['target'] = data[f'mcp_{market_type}'].shift(-96) if market_type == 'dam' else data[f'mcp_{market_type}'].shift(-96 * 2)
         return data
-
 
     def _lags(self, df):
-        data = df.copy()
+        for column in df.columns[1:11]:
+            shift_columns = [df[column].shift(96 * i) for i in range(1, 6)]
+            df = pd.concat([df] + shift_columns, axis=1)
+            df.columns = [*df.columns[:-5], *[f'change_in_{column}_wrt_day_{i}' for i in range(1, 6)]]
 
-        for column in df.columns[1:11]:  
-            shift_columns = [data[column].shift(96 * i) for i in range(1, 6)]
-            data = pd.concat([data] + shift_columns, axis=1)
-            data.columns = [*data.columns[:-5], *[f'change_in_{column}_wrt_day_{i}' for i in range(1, 6)]]
+            for i in [1, 2, 4, 8, 12, 18]:
+                df[f'{column}_lag_{i}h'] = df[column].shift(i * 4)
 
-            for i in [1, 2, 4, 8, 12, 18]: 
-                # hour lags feature
-                data[f'{column}_lag_{i}h'] = data[column].shift(i*4)
-
-        return data
+        return df
 
     def _min_max(self, df):
-        data = df.copy()
-
         for column in df.columns[1:11]:
-            # hour mean
-            hour_mean = data.groupby(['date', 'hour']).mean()[column].reset_index().rename(columns={column: f'hour_mean_{column}'})
-            data = pd.merge(data, hour_mean, on=['date', 'hour'], how='left')    
-            
-            # daily mean
-            daily_mean = data.groupby(['date']).mean()[column].reset_index().rename(columns={column: f'daily_mean_{column}'})
-            data = pd.merge(data, daily_mean, on=['date'], how='left')
-            
-            # daily minimum
-            daily_min = data.groupby(['date']).min()[column].reset_index().rename(columns={column: f'daily_min_{column}'})
-            data = pd.merge(data, daily_min, on=['date'], how='left')
-            
-            # daily maximum
-            daily_max = data.groupby(['date']).max()[column].reset_index().rename(columns={column: f'daily_max_{column}'})
-            data = pd.merge(data, daily_max, on=['date'], how='left')
+            hour_mean = df.groupby(['date', 'hour']).mean()[column].reset_index().rename(columns={column: f'hour_mean_{column}'})
+            df = pd.merge(df, hour_mean, on=['date', 'hour'], how='left')
 
-        return data
+            daily_mean = df.groupby(['date']).mean()[column].reset_index().rename(columns={column: f'daily_mean_{column}'})
+            df = pd.merge(df, daily_mean, on=['date'], how='left')
 
+            daily_min = df.groupby(['date']).min()[column].reset_index().rename(columns={column: f'daily_min_{column}'})
+            df = pd.merge(df, daily_min, on=['date'], how='left')
 
-    # exponential weighted moving averages
+            daily_max = df.groupby(['date']).max()[column].reset_index().rename(columns={column: f'daily_max_{column}'})
+            df = pd.merge(df, daily_max, on=['date'], how='left')
+
+        return df
+
     def _ema(self, df, market_type):
-        data = df.copy()
-        for i in [1,3,6,12]:
-            data[f'ewma_{i}h'] = data[f'mcp_{market_type}'].ewm(span=4*i).mean() 
-            
-        for i in [1,3,5]:
-            data[f'ewma_{i}d'] = data[f'mcp_{market_type}'].ewm(span=96*i).mean()
-        return data
+        for i in [1, 3, 6, 12]:
+            df[f'ewma_{i}h'] = df[f'mcp_{market_type}'].ewm(span=4 * i).mean()
 
+        for i in [1, 3, 5]:
+            df[f'ewma_{i}d'] = df[f'mcp_{market_type}'].ewm(span=96 * i).mean()
+        return df
 
-    # mean
     def _mean(self, df, market_type):
-        data = df.copy()
         window_sizes = [2, 3, 5]
         for window_size in window_sizes:
-            data[f'mcp_{market_type}_mean_{window_size}d'] = data[f'mcp_{market_type}'].rolling(window_size * 96).mean()
-        return data
+            df[f'mcp_{market_type}_mean_{window_size}d'] = df[f'mcp_{market_type}'].rolling(window_size * 96).mean()
+        return df
 
-
-    # interaction with datetime
     def _interaction(self, df, market_type):
-        data = df.copy()
-        data[f'mcp_{market_type}_with_dom'] = data[f'mcp_{market_type}'] * data['dom'] 
-        data[f'mcp_{market_type}_with_month'] = data[f'mcp_{market_type}'] * data['month']
-        data[f'mcp_{market_type}_with_dow'] = data[f'mcp_{market_type}'] * data['dow']
-        data[f'mcp_{market_type}_with_doy'] = data[f'mcp_{market_type}'] * data['doy']
-        data[f'mcp_{market_type}_with_tb'] = data[f'mcp_{market_type}'] * data['tb']
-        return data
+        df[f'mcp_{market_type}_with_dom'] = df[f'mcp_{market_type}'] * df['dom']
+        df[f'mcp_{market_type}_with_month'] = df[f'mcp_{market_type}'] * df['month']
+        df[f'mcp_{market_type}_with_dow'] = df[f'mcp_{market_type}'] * df['dow']
+        df[f'mcp_{market_type}_with_doy'] = df[f'mcp_{market_type}'] * df['doy']
+        df[f'mcp_{market_type}_with_tb'] = df[f'mcp_{market_type}'] * df['tb']
+        return df
 
-
-    # covid features
     def _covid(self, df):
-        data = df.copy()
-        data.loc[(data.year == 2020)&(data.month.isin([3,4,5,6,7,8,9])),'covid_first_wave'] = 1
-        data['covid_first_wave'] = data['covid_first_wave'].replace(np.nan, 0)
+        df.loc[(df.year == 2020) & (df.month.isin([3, 4, 5, 6, 7, 8, 9])), 'covid_first_wave'] = 1
+        df['covid_first_wave'] = df['covid_first_wave'].replace(np.nan, 0)
 
-        # covid 2nd wave
-        data.loc[(data.year == 2021)&(data.month.isin([3,4,5,6])),'covid_second_wave'] = 1
-        data['covid_second_wave'] = data['covid_second_wave'].replace(np.nan, 0)
-        return data
-
+        df.loc[(df.year == 2021) & (df.month.isin([3, 4, 5, 6])), 'covid_second_wave'] = 1
+        df['covid_second_wave'] = df['covid_second_wave'].replace(np.nan, 0)
+        return df
 
     def _cyclic(self, df, feature):
-        data = df.copy()
-        data['norm'] = 2 * math.pi * data[f"{feature}"] / data[f"{feature}"].max()
-        data[f"cos_{feature}"] = np.cos(data["norm"])
-        data[f"sin_{feature}"] = np.sin(data["norm"])
-        data.drop('norm', axis=1, inplace=True)
-        return data
-
+        df['norm'] = 2 * math.pi * df[f"{feature}"] / df[f"{feature}"].max()
+        df[f"cos_{feature}"] = np.cos(df["norm"])
+        df[f"sin_{feature}"] = np.sin(df["norm"])
+        df.drop('norm', axis=1, inplace=True)
+        return df
 
     def _price_features(self, data, market_type, task):
         data = self._capping(data)
@@ -184,35 +142,28 @@ class FeatureEngineering:
         data = self._cyclic(data, 'doy')
         return data
 
-
     def _weather_features(self, data, weather):
         for column in weather.columns[1:65]:
-            # daily mean weather
-            daily_mean = data.groupby(['date']).mean()[column].reset_index().rename(columns = {column:f'daily_mean_{column}'})
+            daily_mean = data.groupby(['date']).mean()[column].reset_index().rename(columns={column: f'daily_mean_{column}'})
             data = pd.merge(data, daily_mean, on=['date'], how='left')
 
-            # difference in weather features
             for i in range(1, 4):
-                    data[f'change_in_{column}_wrt_day_{i}'] = (data[column] - data[column].shift(96*i))
+                data[f'change_in_{column}_wrt_day_{i}'] = (data[column] - data[column].shift(96 * i))
 
-            # total precipitation in each timeblock
             prec_columns = data.filter(regex='^prec_')
             data['prec_tb'] = prec_columns.sum(axis=1)
-        
         return data
-
 
     def _interaction_features(self, data, weather, market_type):
         for i in weather.columns[1:65]:
             data[f'mcp_{market_type}_with_{i}'] = data[f'mcp_{market_type}'] * data[i]
         return data
 
-
-    def _get_features(self, data, weather, market_type, task = 'train'):
+    def _get_features(self, data, weather, market_type, task='train'):
         data = self._price_features(data, market_type, task)
         data = self._weather_features(data, weather)
         data = self._interaction_features(data, weather, market_type)
-        data = data.drop('date', axis=1)    
+        data = data.drop('date', axis=1)
         data = data.dropna()
         data = data.reset_index(drop=True)
         return data
