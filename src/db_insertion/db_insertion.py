2e1286d6aa752bea3a2be7ad49ccb9e4266753cf
aman.bhatt <aman.bhatt@climateconnect.digital>
2024-01-17 02:28:32 +0530

    database insertion for dam and directional

--- src/db_insertion/db_insertion.py
+++ src/db_insertion/db_insertion.py
@@ -31,14 +31,17 @@ class DAMInsertion:
         df['time_block'] = df[['start', 'end']].agg('-'.join, axis=1)
 
         # create label
-        df['label'] = forecast_type  
+        if forecast_type == 'dam_forecast':
+            df['label'] = 'forecast'
+        else:
+            df['label'] = forecast_type  
         df['MCP'] = forecasts[forecast_type]
             
         # Drop the 'start' and 'end' columns
         df.drop(['start', 'end'], axis=1, inplace=True)
 
         # Calculate revision based on forecast_type
-        revision = {'forecast': 0, 'lower_bound': 1, 'upper_bound': 2}.get(forecast_type, -1)
+        revision = {'dam_forecast': 0, 'lower_bound': 1, 'upper_bound': 2}.get(forecast_type, -1)
 
         # Create a dictionary to store the result
         result_dict = {
@@ -66,14 +69,13 @@ class DAMInsertion:
 
     def save_forecast(self, forecasts, forecasting_date, forecast_type):
         forecast_data = self.forecast_dict(forecasts, forecasting_date, forecast_type)  
-        print(forecast_data)
         url = self.base_url + 'savePriceForecast'
         headers = {'Authorization': 'Bearer ' + str(self.iex_data._get_token()), 'Content-Type': 'application/json'}
         response = requests.post(url=url, data=json.dumps(forecast_data), headers=headers)
         if response.json()['status'] == 'success':
-            print(f'{forecast_type} data inserted successfully')
+            print(f'{forecast_type} forecast inserted successfully.')
         else:
-            print(f'{forecast_type} data not inserted')
+            print(f'{forecast_type} forecast not inserted.')
 
 
 class DirInsertion:
@@ -87,34 +89,35 @@ class DirInsertion:
     def forecast_dict(self, forecasts, forecasting_date, forecast_type):
         df = pd.DataFrame()
 
-        # creating block number
-        l = [i for i in range(1, 25) for _ in range(4)]
-        df['block_no'] = pd.Series(l)
+        # creating hour
+        df['datetime'] = pd.date_range(start = str(forecasting_date) + ' 00:00', end = str(forecasting_date) + ' 23:45', freq = '15min')[0:96]
+        df['Hour']=(df['datetime'].dt.hour) + 1
+
+        # creating session id
+        y = [i for i in range(1,49)] + [i for i in range(1,49)]
+        y.sort()
+        df['session_id'] = pd.DataFrame(y)
 
         # create time_block
         df['start'] = pd.date_range(start=f'{forecasting_date} 00:00', end=f'{forecasting_date} 23:45', freq='15min').strftime('%H:%M')
         df['end'] = df['start'].shift(-1).replace(np.nan, '00:00')
         df['time_block'] = df[['start', 'end']].agg('-'.join, axis=1)
 
-        # create label
-        df['label'] = forecast_type  
-        df['MCP'] = forecasts[forecast_type]
+        df['MCP'] = 'MCP'
+        df['MCP'] = forecasts[f'{forecast_type}_forecast']
             
         # Drop the 'start' and 'end' columns
         df.drop(['start', 'end'], axis=1, inplace=True)
 
-        # Calculate revision based on forecast_type
-        revision = {'forecast': 0, 'lower_bound': 1, 'upper_bound': 2}.get(forecast_type, -1)
-
         # Create a dictionary to store the result
         result_dict = {
             'date': datetime.strptime(forecasting_date, '%Y-%m-%d').strftime('%d-%m-%Y'),
-            'revision': revision,
+            'revision': 0,
             'data': {'MCP': {}}
         }
 
-        # Group the DataFrame by 'block_no'
-        grouped = df.groupby('block_no')
+        # Group the DataFrame by hour
+        grouped = df.groupby('Hour')
 
         # Iterate over each group and create the nested dictionary structure
         for block_no, group in grouped:
@@ -122,7 +125,7 @@ class DirInsertion:
                 {
                     'time_block': row['time_block'],
                     'price': row['MCP'],
-                    'label': row['label']
+                    'session_id': row['session_id']
                 }
                 for _, row in group.iterrows()
             ]
@@ -132,11 +135,10 @@ class DirInsertion:
 
     def save_forecast(self, forecasts, forecasting_date, forecast_type):
         forecast_data = self.forecast_dict(forecasts, forecasting_date, forecast_type)  
-        print(forecast_data)
-        url = self.base_url + 'savePriceForecast'
+        url = self.base_url + 'saveRTMPriceForecast'
         headers = {'Authorization': 'Bearer ' + str(self.iex_data._get_token()), 'Content-Type': 'application/json'}
         response = requests.post(url=url, data=json.dumps(forecast_data), headers=headers)
         if response.json()['status'] == 'success':
-            print(f'{forecast_type} data inserted successfully')
+            print(f'{forecast_type} forecast inserted successfully')
         else:
-            print(f'{forecast_type} data not inserted')
+            print(f'{forecast_type} forecast not inserted')
