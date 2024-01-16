2d2903547ec9f15d6a675871e4251ca3514cc27d
aman.bhatt <aman.bhatt@climateconnect.digital>
2024-01-16 18:15:10 +0530

    corrected evaluation

--- deploy/dam_train.py
+++ deploy/dam_train.py
@@ -1,20 +1,24 @@
-'''
-Author: Aman Bhatt
-'''
-
-# Import necessary libraries and modules
-import os
-import sys
-from datetime import datetime
-from dotenv import load_dotenv
+# %%
+import os, sys
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from datetime import datetime
+from dotenv import load_dotenv
+load_dotenv()
 
 PROJECT_PATH = os.getenv('PROJECT_DIR')
 sys.path.append(PROJECT_PATH)
 
-# Custom Modules
+# ignore warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+# visualization
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = (12, 6)
+
+# %%
+# custom modules
 from src.data_ingestion.iex_data import IexDataFetcher
 from src.data_ingestion.weather_data import WeatherDataFetcher
 from src.feature_engineering.build_features import FeatureEngineering
@@ -23,26 +27,21 @@ from src.model_building.eval_model import ModelEvaluator
 from src.utils import *
 from config.paths import *
 
-# Load environment variables
-load_dotenv()
-
-# Set project path and add it to sys.path
-PROJECT_PATH = os.getenv('PROJECT_DIR')
-sys.path.append(PROJECT_PATH)
-
-# Ignore warnings
-import warnings
-warnings.filterwarnings('ignore')
-
-# Set visualization configurations
-plt.rcParams['figure.figsize'] = (12, 6)
-
-# Instantiate data fetchers, feature engineering, and model building
+# %%
+# creating instances
 iex_data = IexDataFetcher()
 weather_data = WeatherDataFetcher()
-featured_data = FeatureEngineering(PROJECT_PATH)
+
+featured_data = FeatureEngineering(PROJECT_PATH) 
 build_model = ModelTraining(PROJECT_PATH)
 
+# %%
+market_type = 'dam'
+
+# %% [markdown]
+# ### Data Ingestion
+
+# %%
 dam = iex_data._get_processed_data('dam')
 rtm = iex_data._get_processed_data('rtm')
 # weather = weather_data._get_processed_weather('weather')
@@ -51,53 +50,85 @@ rtm = iex_data._get_processed_data('rtm')
 # solar = weather_data._get_processed_weather('solar')
 
 # %%
-# dam = load_pickle(PROCESSED_DATA_PATH, 'dam_data')
-# rtm = load_pickle(PROCESSED_DATA_PATH, 'rtm_data')
 weather = load_pickle(PROCESSED_DATA_PATH, 'weather_data')
 wind = load_pickle(PROCESSED_DATA_PATH, 'wind_data')
 hydro = load_pickle(PROCESSED_DATA_PATH, 'hydro_data')
 solar = load_pickle(PROCESSED_DATA_PATH, 'solar_data')
 
-# Merge dataframes
+# %% [markdown]
+# ### Feature Engineering
+
+# %%
+rtm = featured_data.shift_date(rtm, 1) 
+weather = featured_data.shift_date(weather, -1)
+hydro = featured_data.shift_date(hydro, -1) 
+solar = featured_data.shift_date(solar, -1) 
+wind = featured_data.shift_date(wind, -1)
+
 data = featured_data.merge_dataframes([dam, rtm, weather, hydro, solar, wind])
 
-# Get features for training data
-training_data = featured_data._get_features(data, weather, market_type='dam')
-save_pickle(training_data, PROCESSED_DATA_PATH, 'training_data')
+# %%
+training_data = featured_data._get_features(data, weather, market_type)
 
-# Load training data
-training_data = load_pickle(PROCESSED_DATA_PATH, 'training_data')
+# %%
+save_pickle(training_data, PROCESSED_DATA_PATH, f'{market_type}_training_data')
 
-# Set desired cutoff dates for training and validation
-training_upto = training_data.iloc[int(training_data.shape[0] * 0.9)]['datetime'].strftime('%Y-%m-%d')  # 90% data for training
-validation_upto = training_data.iloc[int(training_data.shape[0] * 0.95)]['datetime'].strftime('%Y-%m-%d')  # 5% data for validation
+# %% [markdown]
+# ### Model Building
 
-# Split the data
-X_train, y_train, X_valid, y_valid, X_test, y_test = build_model._split_data(training_data, training_upto, validation_upto)
+# %%
+training_data = load_pickle(PROCESSED_DATA_PATH, f'{market_type}_training_data')
 
-# Hyperparameter tuning
+# %%
+# trail and error
 n_trials = 50
-n_features = 100
-best_features, best_params = build_model._features_n_params(X_train, y_train, X_valid, y_valid, n_trials, n_features)
+n_features = 25
 
-# Set training and validation cutoff dates for final model training
-training_upto = training_data[::96]['datetime'].iloc[-1].strftime('%Y-%m-%d')
+# %%
+best_features, best_params = build_model._features_n_params(training_data, n_trials, n_features)
+
+# %% [markdown]
+# ### Model Training & Evaluation
+
+# %%
+n = 5
+
+# %%
+# training upto this date
+training_upto = training_data['datetime'][::96].iloc[-n].strftime('%Y-%m-%d')
 validation_upto = datetime.now().date().strftime('%Y-%m-%d')
 X_train, y_train, X_test, y_test, X_valid, y_valid = build_model._split_data(training_data, training_upto, validation_upto)
 
-# Train the main model and save
-model = build_model._train_model(X_train, y_train, best_params, best_features, objective='regression')
-save_pickle(model, MODELS_PATH, 'dam_forecast')
+# %%
+model = build_model._train_model(X_train, y_train, best_params, best_features, objective = 'regression')
 
-# Train lower quantile model and save
-lower_model = build_model._train_model(X_train, y_train, best_params, best_features, objective='quantile', alpha=0.1)
-save_pickle(lower_model, MODELS_PATH, 'dam_lower')
+# %%
+if n > X_test[::96].shape[0]:
+    n = X_test[::96].shape[0]
 
-# Train upper quantile model and save
-upper_model = build_model._train_model(X_train, y_train, best_params, best_features, objective='quantile', alpha=0.9)
-save_pickle(upper_model, MODELS_PATH, 'dam_upper')
-
-# Evaluate the model on training and test data
+# %%
 evaluator = ModelEvaluator(model, best_features)
-evaluator.evaluate_on_data(X_train, y_train, 'train', 5)
-# evaluator.evaluate_on_data(X_test, y_test, 'test', 1)
+evaluator.evaluate_on_data(X_test, y_test, n, market_type)
+
+# %% [markdown]
+# ### Final Model
+
+# %%
+# training upto this date
+training_upto = datetime.now().date().strftime('%Y-%m-%d')
+validation_upto = datetime.now().date().strftime('%Y-%m-%d')
+X_train, y_train, X_test, y_test, X_valid, y_valid = build_model._split_data(training_data, training_upto, validation_upto)
+
+# %%
+model = build_model._train_model(X_train, y_train, best_params, best_features, objective = 'regression')
+save_pickle(model, MODELS_PATH, f'{market_type}_forecast')
+
+# %%
+lower_model = build_model._train_model(X_train, y_train, best_params, best_features, objective = 'quantile', alpha = 0.1)
+save_pickle(lower_model, MODELS_PATH, f'{market_type}_lower')
+
+# %%
+upper_model = build_model._train_model(X_train, y_train, best_params, best_features, objective = 'quantile', alpha = 0.9)
+save_pickle(upper_model, MODELS_PATH, f'{market_type}_upper')
+
+
