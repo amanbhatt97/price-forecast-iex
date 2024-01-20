a18cf82545f1075a79c52e5dac6e5c1d3ca81c41
aman.bhatt <aman.bhatt@climateconnect.digital>
2024-01-20 17:37:16 +0530

    correct logs

--- src/data_ingestion/iex_data.py
+++ src/data_ingestion/iex_data.py
@@ -24,9 +24,11 @@ sys.path.append(PROJECT_PATH)
 from config.paths import *
 from src.utils import *
 
+data_logs = configure_logger(LOGS_PATH, 'data.log')
+
 class IexDataFetcher:
     def __init__(self):
-        # Access credentials
+        # api credentials
         self.base_url = os.getenv('base_url')
         self.user_email = os.getenv('email')
         self.user_password = os.getenv('password')
@@ -42,6 +44,7 @@ class IexDataFetcher:
             return req['access_token']
         except Exception as e:
             print("Error occurred while retrieving the access token:", str(e))
+            data_logs.error("Error in retrieving the access token: %s", str(e))
 
     def _get_market_data(self, start_date_str, end_date_str, token, market_type):
         """
@@ -61,7 +64,8 @@ class IexDataFetcher:
             r = requests.get(url=url, headers=headers, params=params)
             return r.json()
         except Exception as e:
-            print("Error occurred while retrieving data:", str(e))
+            print(f"Error occurred while retrieving json data for {market_type}:", str(e))
+            data_logs.error("Error occurred while retrieving json data for %s: %s", market_type, str(e))
 
     def _get_datetime_variables(self, market_type):
         """
@@ -74,12 +78,16 @@ class IexDataFetcher:
             Tuple: Tuple containing start date, end date, start date string,
                    end date string, and historical data.
         """
-        data_historical = load_pickle(PROCESSED_DATA_PATH, f'{market_type}_data')
-        start_date = data_historical['datetime'].iloc[-1] + timedelta(hours=0.25)
-        end_date = start_date + timedelta(days=30)
-        start_date_str = start_date.strftime("%d-%m-%Y")
-        end_date_str = end_date.strftime("%d-%m-%Y")
-        return start_date, end_date, start_date_str, end_date_str, data_historical
+        try:
+            data_historical = load_pickle(PROCESSED_DATA_PATH, f'{market_type}_data')
+            start_date = data_historical['datetime'].iloc[-1] + timedelta(hours=0.25)
+            end_date = start_date + timedelta(days=30)
+            start_date_str = start_date.strftime("%d-%m-%Y")
+            end_date_str = end_date.strftime("%d-%m-%Y")
+            return start_date, end_date, start_date_str, end_date_str, data_historical
+        except Exception as e:
+            print(f"Error while creating datetime variables for {market_type} data.:", str(e))
+            data_logs.error("Error while creating datetime variables for %s data: %s", market_type, str(e))
 
     def _get_raw_data(self, market_type):
         """
@@ -104,7 +112,8 @@ class IexDataFetcher:
                 return raw_data
         except Exception as e:
             print("Error in fetching data:", str(e))
-    
+            data_logs.error("Error in fetching raw %s data: %s", market_type, str(e))
+
     def _get_processed_data(self, market_type):
         """
         Processes raw market data, creates additional features, and saves the results.
@@ -115,50 +124,56 @@ class IexDataFetcher:
         Returns:
             pd.DataFrame: Processed market data.
         """
-        raw_data = self._get_raw_data(market_type)
-        start_date, end_date, start_date_str, end_date_str, data_historical = self._get_datetime_variables(market_type)
+        try:
+            raw_data = self._get_raw_data(market_type)
+            start_date, end_date, start_date_str, end_date_str, data_historical = self._get_datetime_variables(market_type)
 
-        if market_type == 'dam':
-            # Processing steps for DAM data
-            if not raw_data.empty:
-                df = raw_data[['mcp', 'mcv', 'purchase_bid', 'sell_bid']]
-                for column in df.columns:
-                    df.loc[:, column] = pd.to_numeric(df[column], errors='coerce').copy()
-                df = df.rename(columns={'mcp': 'mcp_dam', 'mcv': 'clearedvolume_dam',
-                                        'purchase_bid': 'pb_dam', 'sell_bid': 'sb_dam'})
-                df['diff_sb_pb_dam'] = df['pb_dam'] - df['sb_dam']
-                dates = pd.date_range(start=start_date.date(), end=end_date.date(), freq='15min')[:-1]
-                current_dates = pd.DataFrame({'datetime': dates})
-                current_data = pd.concat([current_dates, df], axis=1).dropna()
-                processed_data = pd.concat([data_historical, current_data]).reset_index(drop=True)
-                last_date = processed_data['datetime'].iloc[-1].strftime('%d-%m-%Y %H:%M')
-                print(f'{market_type} data updated up to: ', last_date)
-                save_pickle(processed_data, PROCESSED_DATA_PATH, f'{market_type}_data')
-                return processed_data
-            else:
-                return data_historical
+            if market_type == 'dam':
+                # Processing steps for DAM data
+                if not raw_data.empty:
+                    df = raw_data[['mcp', 'mcv', 'purchase_bid', 'sell_bid']]
+                    for column in df.columns:
+                        df.loc[:, column] = pd.to_numeric(df[column], errors='coerce').copy()
+                    df = df.rename(columns={'mcp': 'mcp_dam', 'mcv': 'clearedvolume_dam',
+                                            'purchase_bid': 'pb_dam', 'sell_bid': 'sb_dam'})
+                    df['diff_sb_pb_dam'] = df['pb_dam'] - df['sb_dam']
+                    dates = pd.date_range(start=start_date.date(), end=end_date.date(), freq='15min')[:-1]
+                    current_dates = pd.DataFrame({'datetime': dates})
+                    current_data = pd.concat([current_dates, df], axis=1).dropna()
+                    processed_data = pd.concat([data_historical, current_data]).reset_index(drop=True)
+                    last_date = processed_data['datetime'].iloc[-1].strftime('%d-%m-%Y %H:%M')
+                    print(f'{market_type} data updated up to: ', last_date)
+                    save_pickle(processed_data, PROCESSED_DATA_PATH, f'{market_type}_data')
+                    return processed_data
+                else:
+                    return data_historical
 
-        elif market_type == 'rtm':
-            # Processing steps for RTM data
-            if not raw_data.empty:
-                df = raw_data.copy()
-                df['datetime'] = pd.to_datetime(df['date'], format='%d-%m-%Y') + pd.to_timedelta(df['time_block'].str.split('-').str[0] + ':00')
-                df = df[df['datetime'] >= start_date]
-                df = df[['datetime', 'mcp', 'mcv', 'purchase_bid', 'sell_bid']]
-                for column in df.columns[1:]:
-                    df[column] = pd.to_numeric(df[column])
-                df = df.rename(columns={'mcp': 'mcp_rtm', 'mcv': 'clearedvolume_rtm',
-                                        'purchase_bid': 'pb_rtm', 'sell_bid': 'sb_rtm'})
-                df['diff_sb_pb_rtm'] = df['pb_rtm'] - df['sb_rtm']
-                current_data = df.copy()
-                processed_data = pd.concat([data_historical, current_data]).reset_index(drop=True)
-                save_pickle(processed_data, PROCESSED_DATA_PATH, f'{market_type}_data') 
-                last_date = processed_data['datetime'].iloc[-1].strftime('%d-%m-%Y %H:%M')
-                print(f'{market_type} data updated up to: ', last_date)
-                return processed_data
+            elif market_type == 'rtm':
+                # Processing steps for RTM data
+                if not raw_data.empty:
+                    df = raw_data.copy()
+                    df['datetime'] = pd.to_datetime(df['date'], format='%d-%m-%Y') + pd.to_timedelta(df['time_block'].str.split('-').str[0] + ':00')
+                    df = df[df['datetime'] >= start_date]
+                    df = df[['datetime', 'mcp', 'mcv', 'purchase_bid', 'sell_bid']]
+                    for column in df.columns[1:]:
+                        df[column] = pd.to_numeric(df[column])
+                    df = df.rename(columns={'mcp': 'mcp_rtm', 'mcv': 'clearedvolume_rtm',
+                                            'purchase_bid': 'pb_rtm', 'sell_bid': 'sb_rtm'})
+                    df['diff_sb_pb_rtm'] = df['pb_rtm'] - df['sb_rtm']
+                    current_data = df.copy()
+                    processed_data = pd.concat([data_historical, current_data]).reset_index(drop=True)
+                    save_pickle(processed_data, PROCESSED_DATA_PATH, f'{market_type}_data') 
+                    last_date = processed_data['datetime'].iloc[-1].strftime('%d-%m-%Y %H:%M')
+                    print(f'{market_type} data updated up to: ', last_date)
+                    return processed_data
+                else:
+                    last_date = data_historical['datetime'].iloc[-1].strftime('%d-%m-%Y %H:%M')
+                    return data_historical 
+                
             else:
-                last_date = data_historical['datetime'].iloc[-1].strftime('%d-%m-%Y %H:%M')
-                return data_historical 
+                print('Use either "dam" or "rtm" to fetch data.')
+                data_logs.warning('Use either "dam" or "rtm" to fetch data.')
+        except Exception as e:
+            print("Error in processing data:", str(e)) 
+            data_logs.error('Error while processing %s data: %s', market_type, str(e))
             
-        else:
-            print('Use either "dam" or "rtm" to fetch data.')
--- src/data_ingestion/weather_data.py
+++ src/data_ingestion/weather_data.py
@@ -1,5 +1,12 @@
+'''
+This script fetches weather data from the Meteoblue API, processes it, and stores it in pickle files.
+It includes a class `WeatherDataFetcher` with methods to retrieve data, process it, and save the results.
+
+Author: Aman Bhatt
+'''
+
+# Import necessary libraries
 import os
-import time
 import sys
 import yaml
 import requests
@@ -10,32 +17,65 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Access credentials
+# Access Meteoblue API key from environment variables
 api_key = os.getenv('meteoblue_api_key')
 
+# Access project paths
 PROJECT_PATH = os.getenv('PROJECT_DIR')
 sys.path.append(PROJECT_PATH)
 
+# Import utility functions
 from config.paths import RAW_DATA_PATH, PROCESSED_DATA_PATH
 from src.utils import *
+from config.paths import LOGS_PATH
+
+data_logs = configure_logger(LOGS_PATH, 'data.log')
 
 class WeatherDataFetcher:
     def __init__(self):
+        '''
+        Initializes the WeatherDataFetcher class with necessary credentials and paths.
+        '''
         # Access credentials
         self.api_key = os.getenv('meteoblue_api_key')
         self.PROJECT_PATH = os.getenv('PROJECT_DIR')
         sys.path.append(self.PROJECT_PATH)
 
     def _get_weather_response(self, lat, lon, asl, name):
+        '''
+        Sends a request to the Meteoblue API for basic 1-hour weather data.
+
+        Args:
+        - lat: Latitude of the location
+        - lon: Longitude of the location
+        - asl: Altitude above sea level of the location
+        - name: Name of the location
+
+        Returns:
+        - Weather data in JSON format
+        '''
         try:
             url = f"http://my.meteoblue.com/packages/basic-1h?name={name}&lat={lat}&lon={lon}&asl={asl}&tz=UTC&apikey={self.api_key}"
             response = requests.get(url)
             data = response.json()
             return data
         except Exception as e:
-            print("Error occurred while retrieving data:", str(e))
+            print("Error occurred while retrieving json data:", str(e))
+            data_logs.error("Error occurred while retrieving the json data: ", str(e))
 
     def _get_solar_response(self, lat, lon, asl, name):
+        '''
+        Sends a request to the Meteoblue API for solar 15-minute weather data.
+
+        Args:
+        - lat: Latitude of the location
+        - lon: Longitude of the location
+        - asl: Altitude above sea level of the location
+        - name: Name of the location
+
+        Returns:
+        - Solar weather data in JSON format
+        '''
         try:
             url = f"http://my.meteoblue.com/packages/solar-15min?name={name}&lat={lat}&lon={lon}&asl={asl}&tz=UTC&apikey={self.api_key}"
             response = requests.get(url)
@@ -43,67 +83,139 @@ class WeatherDataFetcher:
             return data
 
         except Exception as e:
-            print("Error occurred while retrieving data:", str(e))
+            print("Error occurred while retrieving json data:", str(e))
+            data_logs.error("Error occurred while retrieving json data:", str(e)) 
 
     def _fetch_weather_data(self, locations, required_features, location_type):
-        df_combined = pd.DataFrame()
+        '''
+        Fetches weather data for multiple locations, preprocesses it, and combines into a DataFrame.
 
-        for location in locations:
-            lat, lon, asl, name = location
-            if location_type == 'solar':
-                data_dict = self._get_solar_response(lat, lon, asl, name)
-                df = pd.DataFrame.from_dict(data_dict['data_xmin'])
-            else:
-                data_dict = self._get_weather_response(lat, lon, asl, name)
-                df = pd.DataFrame.from_dict(data_dict['data_1h'])
+        Args:
+        - locations: List of location coordinates and names
+        - required_features: List of features to be included in the processed data
+        - location_type: Type of location data ('solar' or other)
 
-            df = self._preprocess_weather_data(df, required_features, name)
-            df_combined = pd.concat([df_combined, df], ignore_index=True)
-        df_combined = self._resample_and_interpolate(df_combined)
-        save_pickle(df_combined, RAW_DATA_PATH, f'{location_type}')
-        return df_combined
+        Returns:
+        - Combined DataFrame with weather data
+        '''
+        try:
+            df_combined = pd.DataFrame()
+
+            for location in locations:
+                lat, lon, asl, name = location
+                if location_type == 'solar':
+                    data_dict = self._get_solar_response(lat, lon, asl, name)
+                    df = pd.DataFrame.from_dict(data_dict['data_xmin'])
+                else:
+                    data_dict = self._get_weather_response(lat, lon, asl, name)
+                    df = pd.DataFrame.from_dict(data_dict['data_1h'])
+
+                df = self._preprocess_weather_data(df, required_features, name)
+                df_combined = pd.concat([df_combined, df], ignore_index=True)
+            df_combined = self._resample_and_interpolate(df_combined)
+            save_pickle(df_combined, RAW_DATA_PATH, f'{location_type}')
+            return df_combined
+        except Exception as e:
+            print(f'Error while retrieving {location_type} data: ', str(e))
+            data_logs.error('Errorwhile retrieving %s data: ', location_type, str(e))
 
     def _preprocess_weather_data(self, df, required_features, name):
-        cols = required_features
-        df[cols] = df[cols].apply(pd.to_numeric, errors='coerce')
-        df = df.sort_values('time').drop_duplicates(subset=['time'], keep='first')
-        df = df.rename(columns={'time': 'datetime'})
-        df['datetime'] = pd.to_datetime(df['datetime'])
-        df['datetime'] = df['datetime'] + timedelta(hours=5.5)
-        df = df[['datetime'] + required_features]
-        df = df.rename(columns={'windspeed': 'ws', 'ghi_instant': 'ghi',
-                                'relativehumidity': 'rh', 'winddirection': 'wd',
-                                'temperature': 'temp', 'felttemperature': 'atemp', 'precipitation': 'prec'})
-        df['datetime'] = df['datetime'].astype(str)
-        df['location'] = name
-        return df
+        '''
+        Preprocesses raw weather data by converting columns to numeric, handling duplicates, and renaming columns.
+
+        Args:
+        - df: Raw weather data DataFrame
+        - required_features: List of features to be included in the processed data
+        - name: Name of the location
+
+        Returns:
+        - Preprocessed weather data DataFrame
+        '''
+        try:
+            cols = required_features
+            df[cols] = df[cols].apply(pd.to_numeric, errors='coerce')
+            df = df.sort_values('time').drop_duplicates(subset=['time'], keep='first')
+            df = df.rename(columns={'time': 'datetime'})
+            df['datetime'] = pd.to_datetime(df['datetime'])
+            df['datetime'] = df['datetime'] + timedelta(hours=5.5)
+            df = df[['datetime'] + required_features]
+            df = df.rename(columns={'windspeed': 'ws', 'ghi_instant': 'ghi',
+                                    'relativehumidity': 'rh', 'winddirection': 'wd',
+                                    'temperature': 'temp', 'felttemperature': 'atemp', 'precipitation': 'prec'})
+            df['datetime'] = df['datetime'].astype(str)
+            df['location'] = name
+            return df
+        except Exception as e:
+            print(f'Error during preprocessing: ', str(e))
+            data_logs.error('Error during preprocessing: ', str(e)) 
 
     def _resample_and_interpolate(self, df):
-        df = df.pivot(index='datetime', columns='location')
-        df.columns = [f"{col[0]}_{col[1][:3]}" for col in df.columns]
-        df.reset_index(inplace=True)
-        df['datetime'] = pd.to_datetime(df['datetime'])
-        df = df.set_index('datetime').resample('15MIN').interpolate(method='linear').reset_index()
-        return df
+        '''
+        Resamples and interpolates weather data to a 15-minute interval.
+
+        Args:
+        - df: Preprocessed weather data DataFrame
+
+        Returns:
+        - Resampled and interpolated weather data DataFrame
+        '''
+        try:
+            df = df.pivot(index='datetime', columns='location')
+            df.columns = [f"{col[0]}_{col[1][:3]}" for col in df.columns]
+            df.reset_index(inplace=True)
+            df['datetime'] = pd.to_datetime(df['datetime'])
+            df = df.set_index('datetime').resample('15MIN').interpolate(method='linear').reset_index()
+            return df
+        except Exception as e:
+            print(f'Error during resampling and interpolation: ', str(e))
+            data_logs.error('Error during resampling and interpolation: ', str(e))
 
     def _load_locations(self, file_path, location_type):
-        with open(file_path, 'r') as file:
-            locations = yaml.safe_load(file)
-        return locations[location_type]
+        '''
+        Loads location coordinates and names from a YAML file.
+
+        Args:
+        - file_path: Path to the YAML file
+        - location_type: Type of location data ('solar' or other)
+
+        Returns:
+        - Dictionary containing location information
+        '''
+        try:
+            with open(file_path, 'r') as file:
+                locations = yaml.safe_load(file)
+            return locations[location_type]
+        except Exception as e:
+            print(f'Error while fetching {location_type} locations: ',str(e))
+            data_logs.error("Error while fetching %s locations: %s", location_type, str(e)) 
 
     def _get_processed_weather(self, location_type):
-        # Load weather locations from YAML file
-        locations = self._load_locations(os.path.join(self.PROJECT_PATH, 'config', 'locations.yaml'), location_type)
+        '''
+        Retrieves processed weather data, updates historical data, and saves the final DataFrame.
 
-        # Get required features based on location type
-        required_features = locations['required_features']
+        Args:
+        - location_type: Type of location data ('solar' or other)
 
-        raw_df = self._fetch_weather_data(locations['locations'], required_features, location_type)
+        Returns:
+        - Final DataFrame with updated weather data
+        '''
+        try:
+            # Load weather locations from YAML file
+            locations = self._load_locations(os.path.join(self.PROJECT_PATH, 'config', 'locations.yaml'), location_type)
 
-        start_date = raw_df['datetime'].iloc[0]
-        data_historical = pd.read_pickle(os.path.join(PROCESSED_DATA_PATH, f'{location_type}_data'))
-        data_historical = data_historical[data_historical['datetime'] < start_date]
-        final_df = pd.concat([data_historical, raw_df]).reset_index(drop=True)
-        save_pickle(final_df, PROCESSED_DATA_PATH, f'{location_type}_data')
-        print(f'{location_type} data updated.')
-        return final_df
+            # Get required features based on location type
+            required_features = locations['required_features']
+
+            raw_df = self._fetch_weather_data(locations['locations'], required_features, location_type)
+
+            start_date = raw_df['datetime'].iloc[0]
+            data_historical = pd.read_pickle(os.path.join(PROCESSED_DATA_PATH, f'{location_type}_data'))
+            data_historical = data_historical[data_historical['datetime'] < start_date]
+            final_df = pd.concat([data_historical, raw_df]).reset_index(drop=True)
+            save_pickle(final_df, PROCESSED_DATA_PATH, f'{location_type}_data')
+            print(f'{location_type} data updated.')
+            data_logs.info('%s data updated.', location_type)
+            return final_df
+        except Exception as e:
+            print(f'Error during {location_type} processing: ', str(e))
+            data_logs.error('Error during %s processing: ', location_type, str(e))
