65a755eedd2c5a0571219129f40c2419013e62c8
aman.bhatt <aman.bhatt@climateconnect.digital>
2024-01-20 17:37:30 +0530

    add logger

--- src/db_insertion/db_insertion.py
+++ src/db_insertion/db_insertion.py
@@ -1,3 +1,11 @@
+'''
+This script saves forecast values into the database.
+It includes classes `DAMInsertion` and `DirInsertion` with methods to save data in the required format.
+
+Author: Aman Bhatt
+'''
+
+# Import necessary libraries
 import pandas as pd
 import numpy as np
 import requests
@@ -5,13 +13,22 @@ import json
 import os, sys
 from datetime import datetime
 
+# Access project paths
 PROJECT_PATH = os.getenv('PROJECT_DIR')
 sys.path.append(PROJECT_PATH)
 
+from src.utils import *
+from config.paths import *
+forecasting_logs = configure_logger(LOGS_PATH, 'forecasting.log')
+
+# Import IexDataFetcher class for accessing the IEX API
 from src.data_ingestion.iex_data import IexDataFetcher
 
 class DAMInsertion:
     def __init__(self):
+        '''
+        Initializes the DAMInsertion class with necessary credentials and IEX data fetcher.
+        '''
         # Access credentials
         self.base_url = os.getenv('base_url')
         self.user_email = os.getenv('email')
@@ -19,67 +36,94 @@ class DAMInsertion:
         self.iex_data = IexDataFetcher()
 
     def forecast_dict(self, forecasts, forecasting_date, forecast_type):
-        df = pd.DataFrame()
+        '''
+        Creates a dictionary in the required format for DAM (Day-Ahead Market) forecast data.
 
-        # creating block number
-        l = [i for i in range(1, 25) for _ in range(4)]
-        df['block_no'] = pd.Series(l)
+        Args:
+        - forecasts: forecast dataframe
+        - forecasting_date: Date for which the forecast is made
+        - forecast_type: Type of forecast data ('dam_forecast', 'lower_bound', 'upper_bound')
 
-        # create time_block
-        df['start'] = pd.date_range(start=f'{forecasting_date} 00:00', end=f'{forecasting_date} 23:45', freq='15min').strftime('%H:%M')
-        df['end'] = df['start'].shift(-1).replace(np.nan, '00:00')
-        df['time_block'] = df[['start', 'end']].agg('-'.join, axis=1)
+        Returns:
+        - Dictionary in the required format for DAM forecast data
+        '''
+        try:
+            df = pd.DataFrame()
 
-        # create label
-        if forecast_type == 'dam_forecast':
-            df['label'] = 'forecast'
-        else:
-            df['label'] = forecast_type  
-        df['MCP'] = forecasts[forecast_type]
-            
-        # Drop the 'start' and 'end' columns
-        df.drop(['start', 'end'], axis=1, inplace=True)
+            # Creating block number
+            l = [i for i in range(1, 25) for _ in range(4)]
+            df['block_no'] = pd.Series(l)
 
-        # Calculate revision based on forecast_type
-        revision = {'dam_forecast': 0, 'lower_bound': 1, 'upper_bound': 2}.get(forecast_type, -1)
+            # Create time_block
+            df['start'] = pd.date_range(start=f'{forecasting_date} 00:00', end=f'{forecasting_date} 23:45', freq='15min').strftime('%H:%M')
+            df['end'] = df['start'].shift(-1).replace(np.nan, '00:00')
+            df['time_block'] = df[['start', 'end']].agg('-'.join, axis=1)
 
-        # Create a dictionary to store the result
-        result_dict = {
-            'date': datetime.strptime(forecasting_date, '%Y-%m-%d').strftime('%d-%m-%Y'),
-            'revision': revision,
-            'data': {'MCP': {}}
-        }
+            # Create label
+            if forecast_type == 'dam_forecast':
+                df['label'] = 'forecast'
+            else:
+                df['label'] = forecast_type  
+            df['MCP'] = forecasts[forecast_type]
+                
+            # Drop the 'start' and 'end' columns
+            df.drop(['start', 'end'], axis=1, inplace=True)
 
-        # Group the DataFrame by 'block_no'
-        grouped = df.groupby('block_no')
+            # Calculate revision based on forecast_type
+            revision = {'dam_forecast': 0, 'lower_bound': 1, 'upper_bound': 2}.get(forecast_type, -1)
 
-        # Iterate over each group and create the nested dictionary structure
-        for block_no, group in grouped:
-            nested_dict = [
-                {
-                    'time_block': row['time_block'],
-                    'price': row['MCP'],
-                    'label': row['label']
-                }
-                for _, row in group.iterrows()
-            ]
-            result_dict['data']['MCP'][block_no] = nested_dict
+            # Create a dictionary to store the result
+            result_dict = {
+                'date': datetime.strptime(forecasting_date, '%Y-%m-%d').strftime('%d-%m-%Y'),
+                'revision': revision,
+                'data': {'MCP': {}}
+            }
 
-        return result_dict
+            # Group the DataFrame by 'block_no'
+            grouped = df.groupby('block_no')
+
+            # Iterate over each group and create the nested dictionary structure
+            for block_no, group in grouped:
+                nested_dict = [
+                    {
+                        'time_block': row['time_block'],
+                        'price': row['MCP'],
+                        'label': row['label']
+                    }
+                    for _, row in group.iterrows()
+                ]
+                result_dict['data']['MCP'][block_no] = nested_dict
+
+            return result_dict
+        except Exception as e:
+            print('Error while creating forecast dictionary: ', str(e))
+            forecasting_logs.error('Error while creating forecast dictionary: %s', str(e)) 
 
     def save_forecast(self, forecasts, forecasting_date, forecast_type):
+        '''
+        Saves the forecast data into the database.
+
+        Args:
+        - forecasts: forecast dataframe
+        - forecasting_date: Date for which the forecast is made
+        - forecast_type: Type of forecast data ('dam_forecast', 'lower_bound', 'upper_bound')
+        '''
         forecast_data = self.forecast_dict(forecasts, forecasting_date, forecast_type)  
         url = self.base_url + 'savePriceForecast'
         headers = {'Authorization': 'Bearer ' + str(self.iex_data._get_token()), 'Content-Type': 'application/json'}
         response = requests.post(url=url, data=json.dumps(forecast_data), headers=headers)
         if response.json()['status'] == 'success':
             print(f'{forecast_type} forecast inserted successfully.')
+            forecasting_logs.info('%s forecast inserted successfully.', forecast_type)
         else:
             print(f'{forecast_type} forecast not inserted.')
-
+            forecasting_logs.info('%s forecast not inserted.', forecast_type)
 
 class DirInsertion:
     def __init__(self):
+        '''
+        Initializes the DirInsertion class with necessary credentials and IEX data fetcher.
+        '''
         # Access credentials
         self.base_url = os.getenv('base_url')
         self.user_email = os.getenv('email')
@@ -87,58 +131,83 @@ class DirInsertion:
         self.iex_data = IexDataFetcher()
 
     def forecast_dict(self, forecasts, forecasting_date, forecast_type):
-        df = pd.DataFrame()
+        '''
+        Creates a dictionary in the required format for directional forecast data.
 
-        # creating hour
-        df['datetime'] = pd.date_range(start = str(forecasting_date) + ' 00:00', end = str(forecasting_date) + ' 23:45', freq = '15min')[0:96]
-        df['Hour']=(df['datetime'].dt.hour) + 1
+        Args:
+        - forecasts: forecast dataframe
+        - forecasting_date: Date for which the forecast is made
+        - forecast_type: Type of forecast data ('directional_forecast')
 
-        # creating session id
-        y = [i for i in range(1,49)] + [i for i in range(1,49)]
-        y.sort()
-        df['session_id'] = pd.DataFrame(y)
+        Returns:
+        - Dictionary in the required format for directional forecast data
+        '''
+        try:
+            df = pd.DataFrame()
 
-        # create time_block
-        df['start'] = pd.date_range(start=f'{forecasting_date} 00:00', end=f'{forecasting_date} 23:45', freq='15min').strftime('%H:%M')
-        df['end'] = df['start'].shift(-1).replace(np.nan, '00:00')
-        df['time_block'] = df[['start', 'end']].agg('-'.join, axis=1)
+            # Creating hour
+            df['datetime'] = pd.date_range(start = str(forecasting_date) + ' 00:00', end = str(forecasting_date) + ' 23:45', freq = '15min')[0:96]
+            df['Hour']=(df['datetime'].dt.hour) + 1
 
-        df['MCP'] = 'MCP'
-        df['MCP'] = forecasts[f'{forecast_type}_forecast']
-            
-        # Drop the 'start' and 'end' columns
-        df.drop(['start', 'end'], axis=1, inplace=True)
+            # Creating session id
+            y = [i for i in range(1,49)] + [i for i in range(1,49)]
+            y.sort()
+            df['session_id'] = pd.DataFrame(y)
 
-        # Create a dictionary to store the result
-        result_dict = {
-            'date': datetime.strptime(forecasting_date, '%Y-%m-%d').strftime('%d-%m-%Y'),
-            'revision': 0,
-            'data': {'MCP': {}}
-        }
+            # Create time_block
+            df['start'] = pd.date_range(start=f'{forecasting_date} 00:00', end=f'{forecasting_date} 23:45', freq='15min').strftime('%H:%M')
+            df['end'] = df['start'].shift(-1).replace(np.nan, '00:00')
+            df['time_block'] = df[['start', 'end']].agg('-'.join, axis=1)
 
-        # Group the DataFrame by hour
-        grouped = df.groupby('Hour')
+            df['MCP'] = 'MCP'
+            df['MCP'] = forecasts[f'{forecast_type}_forecast']
+                
+            # Drop the 'start' and 'end' columns
+            df.drop(['start', 'end'], axis=1, inplace=True)
 
-        # Iterate over each group and create the nested dictionary structure
-        for block_no, group in grouped:
-            nested_dict = [
-                {
-                    'time_block': row['time_block'],
-                    'price': row['MCP'],
-                    'session_id': row['session_id']
-                }
-                for _, row in group.iterrows()
-            ]
-            result_dict['data']['MCP'][block_no] = nested_dict
+            # Create a dictionary to store the result
+            result_dict = {
+                'date': datetime.strptime(forecasting_date, '%Y-%m-%d').strftime('%d-%m-%Y'),
+                'revision': 1,
+                'data': {'MCP': {}}
+            }
 
-        return result_dict
+            # Group the DataFrame by hour
+            grouped = df.groupby('Hour')
+
+            # Iterate over each group and create the nested dictionary structure
+            for block_no, group in grouped:
+                nested_dict = [
+                    {
+                        'time_block': row['time_block'],
+                        'price': row['MCP'],
+                        'session_id': row['session_id']
+                    }
+                    for _, row in group.iterrows()
+                ]
+                result_dict['data']['MCP'][block_no] = nested_dict
+
+            return result_dict
+        except Exception as e:
+            print('Error while creating forecast dictionary: ', str(e))
+            forecasting_logs.error('Error while creating forecast dictionary: %s', str(e))
 
     def save_forecast(self, forecasts, forecasting_date, forecast_type):
+        '''
+        Saves the forecast data into the database.
+
+        Args:
+        - forecasts: forecast dataframe
+        - forecasting_date: Date for which the forecast is made
+        - forecast_type: Type of forecast data ('directional_forecast')
+        '''
         forecast_data = self.forecast_dict(forecasts, forecasting_date, forecast_type)  
         url = self.base_url + 'saveRTMPriceForecast'
         headers = {'Authorization': 'Bearer ' + str(self.iex_data._get_token()), 'Content-Type': 'application/json'}
         response = requests.post(url=url, data=json.dumps(forecast_data), headers=headers)
         if response.json()['status'] == 'success':
-            print(f'{forecast_type} forecast inserted successfully')
+            print(f'{forecast_type} forecast inserted successfully.')
+            forecasting_logs.info('%s forecast inserted successfully.', forecast_type)
         else:
-            print(f'{forecast_type} forecast not inserted')
+            print(f'{forecast_type} forecast not inserted.')
+            forecasting_logs.info('%s forecast not inserted.', forecast_type)
